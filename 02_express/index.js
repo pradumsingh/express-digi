b8c6af247b55e5d9c0c3ba345cb3f664bb596c17
Pradum Singh <singhpradum17@gmail.com>
2025-06-09 00:03:34 +0530

    index.js

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,7 +1,8 @@
+import 'dotenv/config'
 import express from 'express'
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 // To accept JSON data from frontend
 app.use(express.json())
